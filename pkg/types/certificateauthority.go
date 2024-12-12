e8a78cbaddf01ed44a4375b0582adca152532f41
Dustin Scott <dustin.scott18@gmail.com>
2024-12-12 05:38:39 -0600

    chore: remove commented code
    Signed-off-by: Dustin Scott <dustin.scott18@gmail.com>

--- pkg/types/certificateauthority.go
+++ pkg/types/certificateauthority.go
@@ -42,45 +42,6 @@ func (ca *certificateAuthority) SetRequest(input *CertificateInput) {
 	})
 }
 
-// // GenerateRoot generates the root certificate for the certificate authority and stores the data
-// // in a certificate.
-// func (ca *certificateAuthority) GenerateRoot(input *CertificateAuthorityInput) error {
-// 	// create a new certificate authority certificate from the key
-// 	caCert := &x509.Certificate{
-// 		SerialNumber: big.NewInt(1), // Use a unique serial number for each certificate
-// 		Subject: pkix.Name{
-// 			CommonName: input.CommonName,
-// 			// TODO: we may want this later but just focus on org for name
-// 			// Country:      []string{"US"},
-// 			// Province:     []string{"California"},
-// 			// Locality:     []string{"San Francisco"},
-// 		},
-// 		NotBefore:             time.Now(),
-// 		NotAfter:              time.Now().Add(time.Duration(input.ValidDays) * 24 * time.Hour),
-// 		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
-// 		BasicConstraintsValid: true,
-// 		IsCA:                  true,
-// 		MaxPathLen:            0, // Self-signed CA
-// 	}
-
-// 	// self-sign the certificate
-// 	certBytes, err := x509.CreateCertificate(rand.Reader, caCert, caCert, &ca.Key.PublicKey, ca.Key.PrivateKey)
-// 	if err != nil {
-// 		return fmt.Errorf("error creating certificate authority certificate; %w", err)
-// 	}
-
-// 	// encode the certificate to PEM format
-// 	ca.Certificate.CertificateData = pem.EncodeToMemory(&pem.Block{
-// 		Type:  "CERTIFICATE",
-// 		Bytes: certBytes,
-// 	})
-
-// 	// self-sign the certificate
-// 	ca.Certificate.Generate(caCert, ca.Key, ca)
-
-// 	return nil
-// }
-
 // WriteKey writes certificate authority key data to a file.
 func (ca *certificateAuthority) WriteKey(keyPath string) error {
 	if ca.Key == nil {
